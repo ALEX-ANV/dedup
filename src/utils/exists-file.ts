031c904401bfdd69cf0c49e2d32312325567ec4f
Alexandr Antonovich <alexandrantonovich325@gmail.com>
2024-03-15 21:59:15 +0200

    "Implement getIgnoredDependencies function and tests"

--- src/utils/exists-file.ts
+++ src/utils/exists-file.ts
@@ -1,8 +1,8 @@
-import { access } from 'node:fs/promises';
+import fs from 'node:fs/promises';
 
 export async function exists(path: string) {
   try {
-    await access(path);
+    await fs.access(path);
     return true;
   } catch {
     return false;
